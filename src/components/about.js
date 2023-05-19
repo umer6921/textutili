3f7aa7b38576028d2631d3852afce7c08cda0f00
umer6921 <umerm6921@gmail.com>
2023-05-19 15:38:56 +0500

    git commit "change made"

--- src/components/about.js
+++ src/components/about.js
@@ -49,7 +49,7 @@ export default function (props) {
                     <div className="accordion-item" >
                         <h2 className="accordion-header" id="headingTwo">
                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style={{ backgroundColor: props.mode === 'light' ? "white" : "#2a3036", color: props.mode === 'light' ? "black" : "white" }}>
-                               <strong>Key features</strong>
+                               <strong>Key Features</strong>
                             </button>
                         </h2>
                         <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample" style={{ backgroundColor: props.mode === 'light' ? "white" : "#2a3036", color: props.mode === 'light' ? "black" : "white" }}>
@@ -65,7 +65,7 @@ export default function (props) {
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="headingThree">
                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style={{ backgroundColor: props.mode === 'light' ? "white" : "#2a3036", color: props.mode === 'light' ? "black" : "white" }}>
-                               <strong>Why to use?</strong>
+                               <strong>Why to Use?</strong>
                             </button>
                         </h2>
                         <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample" style={{ backgroundColor: props.mode === 'light' ? "white" : "#2a3036", color: props.mode === 'light' ? "black" : "white" }}>
