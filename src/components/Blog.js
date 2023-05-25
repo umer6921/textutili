1adefc8356ca3e24330229d627230f2b1b1fa9b2
umer6921 <umerm6921@gmail.com>
2023-05-25 13:16:29 +0500

    Updated File

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -17,10 +17,10 @@ export default function Blog(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                <Link className="nav-link" aria-current="page" to="/">Home</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/about">{props.about}</Link>
+                <Link className="nav-link" aria-current="page" to="/about">{props.about}</Link>
               </li>
 
 
